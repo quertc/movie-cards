5be5923a3e00a3d257ba83c6faffb68948502e80
Artemy <48246993+quertc@users.noreply.github.com>
2021-02-07 23:53:25 +0300

    refactor: remove unnecessary redirect from /

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,6 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    redirect: '/discover/popular',
-  },
-  {
     path: '/discover/:category',
     name: 'discover',
     component: () => import('@/views/Discover.vue'),
